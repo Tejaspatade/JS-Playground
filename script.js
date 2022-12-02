ab171653e194afb043413fb50376baeb5de461fa
Tejas Patade <tej.patade69@gmail.com>
2022-12-02 20:16:54 +0530

    Coding Challenge 1 Functions

--- script.js
+++ script.js
@@ -47,16 +47,79 @@ greet("Hallo")("Vater");
 
 const greet1 = (greeting) => (name) => console.log(`${greeting}, ${name}!!`);
 
-//
+// Call & Apply methods
 const lufthansa = {
     name: "Lufthansa",
     code: "LH",
     bookings: [],
-    book(flightNum, name) {
+    book(flightNum, username) {
         console.log(
-            `${name} booked flight on ${this.name} ${this.code}${flightNum}`
+            `${username} booked flight on ${this.name} ${this.code}${flightNum}`
         );
-        this.bookings.push({ flight: `${this.code}${flightNum}`, name });
+        this.bookings.push({ flight: `${this.code}${flightNum}`, username });
     },
 };
 lufthansa.book(345, "Tejas");
+
+const euroair = {
+    name: "EuroAir",
+    code: "EA",
+    bookings: [],
+};
+// Globalising book method
+const book = lufthansa.book;
+// Doesnt Work
+// book(23, "Anna");
+book.call(euroair, 23, "Anna");
+
+console.log(lufthansa);
+console.log(euroair);
+
+// bind()
+const bookEA = book.bind(euroair);
+lufthansa.planes = 300;
+lufthansa.buyPlane = function () {
+    this.planes++;
+    console.log(this);
+};
+//  Event listener
+document
+    .querySelector(".buy")
+    .addEventListener("click", lufthansa.buyPlane.bind(lufthansa));
+// Partial Application
+const addTax = (tax, income) => income + income * tax;
+console.log(addTax(0.3, 1000000));
+const addVAT = addTax.bind(null, 0.33);
+
+// CC #1
+const poll = {
+    question: "What is your favourite programming language?",
+    options: ["0: JavaScript", "1: Python", "2: Rust", "3:C++"],
+    // This generates [0, 0, 0, 0]. More in the next section!
+    answers: new Array(4).fill(0),
+    registerNewAnswer() {
+        const ans = Number(
+            prompt(
+                `${this.question} \n${this.options.join(
+                    "\n"
+                )} \n(Write Option Number)`
+            )
+        );
+
+        typeof ans === "number" &&
+            ans < this.answers.length &&
+            this.answers[ans]++;
+        this.displayResults();
+        this.displayResults("string");
+    },
+    displayResults(type = "array") {
+        if (type === "array") {
+            console.log(this.answers);
+        } else if (type === "string") {
+            console.log(`Poll results are ${this.answers.join(", ")}`);
+        }
+    },
+};
+document
+    .querySelector(".poll")
+    .addEventListener("click", poll.registerNewAnswer.bind(poll));
