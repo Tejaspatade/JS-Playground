ea81edbb7d2975ba03df23f5827bb89555dd3f6a
Tejas Patade <tej.patade69@gmail.com>
2022-12-23 19:16:42 +0530

    OOP Setters Getters

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 "use strict";
-
+/*
 // Constructor
 function Constructor(firstName, birthYr) {
     // Instance Properties
@@ -30,3 +30,67 @@ console.log(Constructor.prototype.isPrototypeOf(Constructor));
 Constructor.prototype.species = "Homo Sapiens";
 console.log(obj.species);
 console.log(obj.hasOwnProperty("species"));
+
+// CC#1
+// 1.
+const Car = function (make, speed) {
+    this.make = make;
+    this.speed = speed;
+};
+
+// 2.
+Car.prototype.accelerate = function () {
+    this.speed += 10;
+    console.log(`${this.make} is Running at ${this.speed} km/hr.`);
+};
+
+// 3.
+Car.prototype.brake = function () {
+    this.speed -= 5;
+    console.log(`${this.make} is Running at ${this.speed} km/hr.`);
+};
+
+// 4.
+const mybmw = new Car("BMW", 100);
+const mercedes = new Car("Mercedes", 69);
+mybmw.accelerate();
+mybmw.brake();
+mybmw.accelerate();
+
+mercedes.brake();
+mercedes.brake();
+mercedes.brake();
+mercedes.brake();
+mercedes.accelerate();
+*/
+
+// ES6 Classes
+class Person {
+    // Constructor invoked when new obj is instantiated
+    constructor(fullname, birthYr) {
+        // Setter invoked for fullName
+        this.fullName = fullname;
+        this.birthYr = birthYr;
+    }
+    // Instance Methods
+    calcAge() {
+        console.log(2022 - this.birthYr);
+    }
+    // Gettter
+    get age() {
+        return 2022 - this.birthYr;
+    }
+    // Setter
+    set fullName(name) {
+        if (name.includes(" ")) this._fullName = name;
+        else alert(`${name} is an invalid Name.`);
+    }
+    // Getter
+    get fullName() {
+        return this._fullName;
+    }
+}
+
+const tejas = new Person("Tejas Patade", 2002);
+tejas.calcAge();
+console.log(tejas.age);
