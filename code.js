350e9dcde3b8685986336bac1d7de8df7cd407fd
Tejas Patade <tej.patade69@gmail.com>
2022-12-25 21:23:06 +0530

    OOP Finished

--- code.js
+++ code.js
@@ -1022,3 +1022,221 @@ const observer = new IntersectionObserver(callback, options);
 // Target to be observed
 observer.observe(section1);
 */
+("use strict");
+/*
+// Constructor
+function Constructor(firstName, birthYr) {
+    // Instance Properties
+    this.firstName = firstName;
+    this.birthYr = birthYr;
+}
+
+// Constructor("Simple", 21);
+const obj = new Constructor("Tejas", 2002);
+// 1. New {} object is created
+// 2. Constructor() is called ad this -> {}
+// 3. {} is linked to Constructor.prototype
+// 4. {} is returned implicitly
+console.log(obj);
+
+// Prototypal Inheritance
+console.log(Constructor.prototype);
+Constructor.prototype.calcAge = function () {
+    return 2022 - this.birthYr;
+};
+
+console.log(obj.calcAge());
+console.log(obj.__proto__);
+console.log(obj.__proto__ === Constructor.prototype);
+console.log(Constructor.prototype.isPrototypeOf(obj));
+console.log(Constructor.prototype.isPrototypeOf(Constructor));
+
+Constructor.prototype.species = "Homo Sapiens";
+console.log(obj.species);
+console.log(obj.hasOwnProperty("species"));
+
+// CC#1
+// 1.
+const Car = function (make, speed) {
+    this.make = make;
+    this.speed = speed;
+};
+
+// 2.
+Car.prototype.accelerate = function () {
+    this.speed += 10;
+    console.log(`${this.make} is Running at ${this.speed} km/hr.`);
+};
+
+// 3.
+Car.prototype.brake = function () {
+    this.speed -= 5;
+    console.log(`${this.make} is Running at ${this.speed} km/hr.`);
+};
+
+// 4.
+const mybmw = new Car("BMW", 100);
+const mercedes = new Car("Mercedes", 69);
+mybmw.accelerate();
+mybmw.brake();
+mybmw.accelerate();
+
+mercedes.brake();
+mercedes.brake();
+mercedes.brake();
+mercedes.brake();
+mercedes.accelerate();
+
+// CC#2
+// 1.
+class Car {
+    constructor(make, speed) {
+        this.make = make;
+        this.speed = speed;
+    }
+    
+    // Instance Methods
+    accelerate() {
+        this.speed += 10;
+        console.log(`${this.make} is Running at ${this.speed} km/hr.`);
+    }
+    brake() {
+        this.speed -= 5;
+        console.log(`${this.make} is Running at ${this.speed} km/hr.`);
+    }
+    
+    // Getter
+    get speedUS() {
+        return this.speed / 1.6;
+    }
+    set speedUS(speed) {
+        this.speed = speed * 1.6;
+    }
+}
+
+// ES6 Classes
+class Person {
+    // Constructor invoked when new obj is instantiated
+    constructor(fullname, birthYr) {
+        // Setter invoked for fullName
+        this.fullName = fullname;
+        this.birthYr = birthYr;
+    }
+    // Instance Methods: Accessible to all objects
+    calcAge() {
+        console.log(2022 - this.birthYr);
+    }
+    // Gettter
+    get age() {
+        return 2022 - this.birthYr;
+    }
+    // Setter
+    set fullName(name) {
+        if (name.includes(" ")) this._fullName = name;
+        else alert(`${name} is an invalid Name.`);
+    }
+    // Getter
+    get fullName() {
+        return this._fullName;
+    }
+    // Static Method: Accesible only to the class
+    static hey() {
+        console.log("Helloww!");
+    }
+}
+
+const tejas = new Person("Tejas Patade", 2002);
+tejas.calcAge();
+console.log(tejas.age);
+
+// Object.create()
+// Object to be used as protoype
+const PersonProto = {
+    calcAge() {
+        console.log(2022 - this.birthYr);
+    },
+};
+
+// New person obj that has PersonProto obj as it protoype
+const raju = Object.create(PersonProto);
+console.log(raju);
+
+
+// Inheritance
+// function Person(firstName, birthYr) {
+//     // Instance Properties
+//     this.firstName = firstName;
+//     this.birthYr = birthYr;
+// }
+// Person.prototype.calcAge = function () {
+    //     return 2022 - this.birthYr;
+    // };
+    // // Student Inherits Person
+    // function Student(firstName, birthYr, course) {
+        //     // Calling constructor for a Person
+        //     Person.call(this, firstName, birthYr);
+        //     this.course = course;
+        // }
+        // Student.prototype.introduce = function () {
+            //     console.log(`My Name is ${this.firstName}, I Study ${this.course}`);
+            // };
+            
+            // const om = new Student("Om Shinde Desai", 2002, "CS");
+
+// Inheritance ES6
+class Person {
+    // Constructor invoked when new obj is instantiated
+    constructor(fullname, birthYr) {
+        // Setter invoked for fullName
+        this.fullName = fullname;
+        this.birthYr = birthYr;
+    }
+    // Instance Methods: Accessible to all objects
+    calcAge() {
+        console.log(2022 - this.birthYr);
+    }
+    // Gettter
+    get age() {
+        return 2022 - this.birthYr;
+    }
+    // Setter
+    set fullName(name) {
+        if (name.includes(" ")) this._fullName = name;
+        else alert(`${name} is an invalid Name.`);
+    }
+    // Getter
+    get fullName() {
+        return this._fullName;
+    }
+    // Static Method: Accesible only to the class
+    static hey() {
+        console.log("Helloww!");
+    }
+}
+class Student extends Person {
+    constructor(fullname, birthYr, course) {
+        super(fullname, birthYr);
+        this.course = course;
+    }
+}
+class Account {
+    constructor(owner, currency, pin) {
+        this.owner = owner;
+        this.currency = currency;
+        this.pin = pin;
+        this.movements = [];
+    }
+    
+    // Public Interface
+    deposit(amount) {
+        this.movements.push(amount);
+    }
+    
+    withdrawal(amount) {
+        this.deposit(-amount);
+    }
+}
+
+const acc1 = new Account("Tejas", "INR", 6969);
+
+*/
